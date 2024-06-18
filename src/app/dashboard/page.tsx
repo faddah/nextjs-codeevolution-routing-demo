a22205622117301c8cae313203717a945a2640a7
Faddah Wolf <my_biz@me.com>
2024-06-18 15:44:37 -0700

    Created Dashboard page for dashboard routing folder along with BarChart functional component to use.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import LineChart from "./line-chart";
+
+function BarChart() {
+	return <h2>Bar Chart</h2>;
+}
+
+export default function Dashboard() {
+	return (
+		<>
+			<h1 style={{textDecoration: "underline"}}>Dashboard</h1>
+			<BarChart />
+			<LineChart />
+		</>
+	);
+}
