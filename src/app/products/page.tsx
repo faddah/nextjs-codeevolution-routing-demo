9ccf07cbfbe7fe7ef6512de958fd844892d4148c
Faddah Wolf <my_biz@me.com>
2024-06-18 15:45:43 -0700

    Created ProductsList page for products routing folder.

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+	return (
+		<>
+			<h1>Welcome to Our Marvelous Array of Products!</h1>
+			<h2>Product List</h2>
+			<h3>Product 1</h3>
+			<h3>Product 2</h3>
+			<h3>Product 3</h3>
+		</>);
+}
