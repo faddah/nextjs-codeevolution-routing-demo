a54f453800a89b8f4ac89e06b130ddb372786317
Faddah Wolf <my_biz@me.com>
2024-06-25 15:17:48 -0700

    Added 2nd if-then check of being < 0 as well as > 1000.

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,8 @@
 import { notFound } from "next/navigation";
 
 export default function Review({ params }: { params: { productId: string, reviewId: string } }) {
-	if (parseInt(params.reviewId) > 1000) {
+	const prodId = parseInt(params.productId);
+	if (prodId > 1000 || prodId < 0) {
 		return notFound();
 	}
 	return <h1>Review {params.reviewId} for Product {params.productId}</h1>;
