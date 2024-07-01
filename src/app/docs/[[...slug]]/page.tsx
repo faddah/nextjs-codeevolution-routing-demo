bfe960569f4f2e47a18db047ac5437976731d1fb
Faddah Wolf <my_biz@me.com>
2024-07-01 07:26:56 -0700

    Changed catch-all segments in docs page.tsx to be a switch statement.

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -5,10 +5,15 @@ export default function Docs({
 		slug: string[];
 	};
 }) {
-	if (params.slug?.length === 2) {
-		return <h1>Viewing Docs for Feature {params.slug[0]} and Concept {params.slug[1]}.</h1>;
-	} else if (params.slug?.length === 1) {
-		return <h1>Viewing Docs for Feature {params.slug[0]}.</h1>;
+	// Re-write the following if - else as a switch statement
+	switch (params.slug?.length) {
+		case 3:
+			return <h1>Viewing Docs for Feature {params.slug[0]}, Concept {params.slug[1]}, and Example {params.slug[2]}.</h1>;
+		case 2:
+			return <h1>Viewing Docs for Feature {params.slug[0]} and Concept {params.slug[1]}.</h1>;
+		case 1:
+			return <h1>Viewing Docs for Feature {params.slug[0]}.</h1>;
+		default:
+			return <h1>Docs Home Page</h1>;
 	}
-	return <h1>Docs Home Page</h1>;
 }
