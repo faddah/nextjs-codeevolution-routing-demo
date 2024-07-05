509d0335aa31183d043c45a9ee9170e7758ad9c7
Faddah Wolf <my_biz@me.com>
2024-07-04 17:25:19 -0700

    Created Login, Register & ForgotPassword pages nested in route group (auth) folder.

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function ForgotPassword(): JSX.Element {
+	return (<h1>Help! I forgot my password!</h1>);
+}
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Login(): JSX.Element {
+	return (<h1>Login</h1>);
+}
