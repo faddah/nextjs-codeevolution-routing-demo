9e0dd5c0703992bfb9b34f031882a8907da60b5a
Faddah Wolf <my_biz@me.com>
2024-07-04 17:39:34 -0700

    Added about folder & page w/ imported Image of office buildings.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function About(): JSX.Element {
+	return (
+		<div style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "1rem" }}>
+			<h1>About Our Company</h1>
+			<Image
+				src="/glass-office-towers.jpg"
+				alt="About Image - Glass Office Towers"
+				width={500}
+				height={400}
+				priority
+			/>
+		</div>
+	);
+}
