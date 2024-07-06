5c8bce4f0faf6904c3ba113caa9469d1a2a7bbc2
Faddah Wolf <my_biz@me.com>
2024-07-06 14:19:38 -0700

    Imported Metadata type & added to const metadata with a default title.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,12 @@
 import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: {
+		default: 'Our Blog',
+	},
+	description: 'The Faddah Company Blog',
+}
 
 export default function Blog(): JSX.Element {
 	return (
