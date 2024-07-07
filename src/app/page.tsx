e7f85cdb372d7d38e273d52093565931673f111b
Faddah Wolf <my_biz@me.com>
2024-07-06 17:13:08 -0700

    Imported Link & added links at bottom to about, product & blog pages also centered page using flex.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HomePage() {
 	return (
-		<div>
+		<div style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "1rem" }}>
 			<h1>Welcome Home!</h1>
 
 			<div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
@@ -17,6 +18,12 @@ export default function HomePage() {
 			</div>
 
 			<h2>I&apos;M PICKLE RICK! WUBBA LUBBA DUB DUB!</h2>
+
+			<ul>
+				<li><Link href="/about">Our About Pages</Link></li>
+				<li><Link href="/products">Our Producs</Link></li>
+				<li><Link href="/blog">Our Blog</Link></li>
+			</ul>
 		</div>
 	);
 }
