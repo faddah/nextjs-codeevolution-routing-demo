888495131b0544b9718cab60d28274fc0841a827
Faddah Wolf <my_biz@me.com>
2024-07-06 17:14:32 -0700

    Imported Link and added dynamic Links to each product plus a productId product.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function ProductList() {
+	const productId = 101
 	return (
 		<>
 			<h1>Welcome to Our Marvelous Array of Products!</h1>
 			<h2>Product List</h2>
-			<h3>Product 1</h3>
-			<h3>Product 2</h3>
-			<h3>Product 3</h3>
+			<h3><Link href="products/1">Product 1</Link></h3>
+			<h3><Link href="products/2">Product 2</Link></h3>
+			<h3><Link href="products/3" replace>Product 3</Link></h3>
+			<h3><Link href={`products/${productId}`}>Product {productId}</Link></h3>
+			<Link href="/">Go Home</Link>
 		</>);
 }
